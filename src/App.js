8bc2d677c84c2693939f60e2258f7e393e7d4d22
Jan Hickiewicz <janhi@tutanota.com>
2023-02-15 23:47:36 +0100

    move ThemeProvider to App component for switching there between light and dark modes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import { Container } from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
 import Section from "./components/Section";
+import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const isDarkMode = mode === "dark";
+
+  const toggleMode = () => setMode(isDarkMode ? "light" : "dark");
+
   return (
-    <Container>
-      <Header />
-      <Section title={"My skillset"} />
-      <Section title={"What I want to learn next"} />
-      <Portfolio />
-      <Footer />
-    </Container>
+    <ThemeProvider theme={isDarkMode ? theme.darkMode : theme.lightMode}>
+      <GlobalStyle />
+      <Container>
+        <Header />
+        <Section title={"My skillset"} />
+        <Section title={"What I want to learn next"} />
+        <Portfolio />
+        <Footer />
+      </Container>
+    </ThemeProvider>
   );
 }
 

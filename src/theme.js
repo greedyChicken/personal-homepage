872bfc4ec03164551c4520827dae491141a83cdc
Jan Hickiewicz <janhi@tutanota.com>
2023-03-19 18:40:49 +0100

    change theme for dark and light modes

--- src/theme.js
+++ src/theme.js
@@ -11,13 +11,20 @@ export const theme = {
       projectBorder: "rgba(209, 213, 218, 0.3)",
       projectBorderHover: "rgba(3, 102, 214, 0.2)",
       socialMedia: "#252525",
+      togglerBackground: "#E5E5E5",
+      hireMeButtonBorder: "1px solid rgba(209, 213, 218, 0.3)",
+      hireMeButtonHoverShadow:
+        "2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;",
+      emailHover: "#2188FF",
+      icon: "#FFFFFF",
+      spinnerMain: "#D1D5DA4D",
     },
   },
   darkMode: {
     color: {
       background: "#252525",
       title: "#FFFFFF",
-      sectionBackground: "#FFFFFF",
+      sectionBackground: "rgba(54, 54, 54, 0.72)",
       text: "#FFFFFF",
       portfolioItemTitle: "#FFFFFF",
       link: "#2188FF",
@@ -25,14 +32,13 @@ export const theme = {
       projectBorder: "rgba(209, 213, 218, 0.1)",
       projectBorderHover: "rgba(3, 102, 214, 0.5)",
       socialMedia: "#FFFFFF",
+      togglerBackground: "#363636",
+      hireMeButtonBorder: "1px solid rgba(209, 213, 218, 0.3)",
+      hireMeButtonHoverShadow:
+        "2px -2px 0px #6D93BE, -2px 2px 0px #6D93BE, 2px 2px 0px #6D93BE, -2px -2px 0px #6D93BE",
+      emailHover: "#0366D6",
+      icon: "#252525",
+      spinnerMain: "#363636",
     },
   },
-
-  // white: "#FFFFFF",
-  // whiteLilac: "#FBFBFE",
-  // iron: "#D1D5DA",
-  // mercury: "#E5E5E5",
-  // slateGray: "#6E7E91",
-  // mineShaft: "#252525",
-  // scienceBlue: "#0366D6",
 };

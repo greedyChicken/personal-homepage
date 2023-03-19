23e5c17c3db70d0deb082a127641c08ebc3c08d0
Jan Hickiewicz <janhi@tutanota.com>
2023-03-19 18:48:14 +0100

    make required changes in App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { ThemeProvider } from "styled-components";
 import { Container } from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ModeToggler from "./components/ModeToggler";
 import Portfolio from "./components/Portfolio";
 import Section from "./components/Section";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import { skills, futureSkills } from "./skills";
+import { SingleSkill } from "./components/SingleSkill";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -18,9 +21,18 @@ function App() {
     <ThemeProvider theme={isDarkMode ? theme.darkMode : theme.lightMode}>
       <GlobalStyle />
       <Container>
+        <ModeToggler toggleMode={toggleMode} isDarkMode={isDarkMode} />
         <Header />
-        <Section title={"My skillset"} />
-        <Section title={"What I want to learn next"} />
+        <Section title={"My skillset"}>
+          {skills.map((skill, index) => (
+            <SingleSkill key={index}>{skill}</SingleSkill>
+          ))}
+        </Section>
+        <Section title={"What I want to learn next"}>
+          {futureSkills.map((skill, index) => (
+            <SingleSkill key={index}>{skill}</SingleSkill>
+          ))}
+        </Section>
         <Portfolio />
         <Footer />
       </Container>
